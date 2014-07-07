109588a459e187cad6a8c952b242a25c5d835d1c
unknown <bethke@ag-bluhm-12.institut2b.physik.rwth-aachen.de>
2014-07-07 11:27:38 +0200

    fix schroedinger solver

--- exercise7/problem.py
+++ exercise7/problem.py
@@ -1,14 +1,25 @@
 #-*- coding: utf-8 -*-
 """
-simple schrödinger
+simple schroedinger
 @author: patrick
 """
 
 from schrodinger import Schrodinger1
 from pylab import *
+import numpy as np
 import matplotlib.animation as animation
-#from util import set_plot_options
-#set_plot_options()
+from util import set_plot_options
+set_plot_options()
+
+
+
+def get_k_space(psi, delta):
+    fourier = np.fft.fft(psi)
+    L = psi.size
+    freq = np.fft.fftfreq(L, delta)
+    freq = np.fft.fftshift(freq)
+    fourier = np.fft.fftshift(fourier)
+    return freq, fourier
 
 #Prepare lattice, potential and wavefunction
 delta = 0.1
@@ -22,38 +33,102 @@ x = np.arange(L)*delta
 #wave packet
 x0 = 20
 q = 1
-sigma = 3
-psi = np.arange(L,dtype='complex')*delta - x0
-psi = (2*np.pi*sigma**2)**(-0.25) * np.exp(1j*-q*psi) * np.exp(-psi**2 / (4*sigma)**2) # added factor -1 in the first exponential
+sigma = 3.
+#psi = np.arange(L,dtype='complex')*delta - x0
+#psi = (2*np.pi*sigma**2)**(-0.25) * np.exp(1j*-q*psi) * np.exp(-psi**2 / (4*sigma)**2) # added factor -1 in the first exponential
+x = x-x0
+psi = (2*np.pi*sigma**2)**(-0.25) * np.exp(1j*x*q) * np.exp(-x**2 / (4*sigma**2))
 
 #potential
 V = np.zeros(L)
-V[500:505] = 2
+V[500:505] = 2.
 
-#Solver instance
-s = Schrodinger1(psi, V, tau, delta)
+
+#Solver instances
+s = Schrodinger1(psi, V, tau, delta) # With tunnel barrier
+s2 = Schrodinger1(psi, np.zeros(L), tau, delta) # without tunnel barrier
+# This works, because psi is not altered in-place
+
+times = np.arange(6)*10.
+steps = diff(times)/tau
 
 #some simple plotting
-
-vscale = 1.0/np.max(V)
-xscale = np.ones(L)*3
+vscale = 1.
+xscale = np.ones(L)
 #xscale[551:] *= 100
-times = []
-probabilities = []
-probabilities.append(s.probability())
-times.append(s.T)
-for i in range(100):
-    s.evolve(500)
-    probabilities.append(s.probability())
-    times.append(s.T)
 
 f = figure()
-images = []
-plot(x, vscale*V, lw=2, color='black')
-for t, psi in zip(times, probabilities):
-    line, = plot(x, xscale*psi, color='b')
-    images.append([line])
-im_ani = animation.ArtistAnimation(f, images, interval=100, blit=True)
+ax = f.add_subplot(111)
+freqs = []
+fts = []
+ax.plot(x, s2.probability(), label='$t = %.1f$' % s2.T)
+for n in steps:
+    s2.evolve(n)
+    ax.plot(x, s2.probability(), label='$t=%.1f$' % s2.T)
 
+ax.legend()
+ax.set_ylim(0, 0.2)
+ax.set_xlabel('$x-x_0$')
+ax.set_ylabel(r'$\left| \Psi(x) \right|^2$')
+ax.set_title('Free Propagation')
+f.savefig('latex/images/free.pdf')
+
+
+f2 = figure()
+ax1 = f2.add_subplot(111)
+ax2 = ax1.twinx()
+ax2.plot(x, vscale*V, lw=2, color='black')
+ax1.plot(x, s.probability()*xscale, label='$t=%d$' % s.T)
+
+fr,ft = get_k_space(s.get_psi(), delta)
+freqs.append(fr)
+fts.append(ft)
+for n in steps:
+    s.evolve(n)
+    ax1.plot(x, s.probability()*xscale, label='$t=%.1f$' % s.T)
+
+fr,ft = get_k_space(s.get_psi(), delta)
+freqs.append(fr)
+fts.append(ft)
+
+ax1.set_xlabel('$x - x_0$')
+ax1.set_ylim(0, 0.2)
+ax1.legend()
+ax2.set_ylabel('Potential $V$')
+ax1.set_title('Tunneling Barrier')
+ax1.set_ylabel(r'$\left| \Psi(x) \right|^2$')
+f2.savefig('latex/images/tunneling.pdf')
+
+f3 = figure()
+ax3 = f3.add_subplot(111)
+freq = freqs[0] * 2*np.pi # go from frequency to angular frequency
+for i,ft in enumerate(fts):
+    plot(freq, np.abs(ft), label='$t=%.1f$' % times[i])
+ax3.set_xlabel('$q$')
+ax3.legend()
+ax3.vlines(1,0,40)
+ax3.set_ylabel('Magnitude')
+ax3.set_title('Momentum Space')
+ax3.set_xlim(-2,2)
+ax3.vli
+f3.savefig('latex/images/fft.pdf')
 show()
+# #vscale = 1.0/np.max(V)
+# times = []
+# probabilities = []
+# probabilities.append(s.probability())
+# times.append(s.T)
+# for i in range(100):
+#     s.evolve(500)
+#     probabilities.append(s.probability())
+#     times.append(s.T)
+
+ #f = figure()
+ #images = []
+ #plot(x, vscale*V, lw=2, color='black')
+ #for t, psi in zip(times, probabilities):
+ #    line, = plot(x, xscale*psi, color='b')
+ #    images.append([line])
+ #ylim(0,0.14)
+ #im_ani = animation.ArtistAnimation(f, images, interval=100, blit=True)
 
--- exercise7/schrodinger.pyx
+++ exercise7/schrodinger.pyx
@@ -1,6 +1,6 @@
  # -*- coding: utf-8 -*-
 """
-diffusion
+1D Schrödinger equation solver
 
 @author: bethke
 """
@@ -21,7 +21,6 @@ cdef inline void mydot(complex[:,:] mat, complex[:] vec) nogil:
     vec[0] = park * mat[0,0] + vec[1] * mat[0,1]
     vec[1] = park * mat[1,0] + vec[1] * mat[1,1]
 
-
 @cython.boundscheck(False)
 cdef class Schrodinger1:
     '''Schroedinger Solver in 1D for constant potential'''
@@ -40,15 +39,16 @@ cdef class Schrodinger1:
         self.psi = psi_init.copy()
         self.L = psi_init.size
         self.even = (self.L % 2 == 0)
+        self.delta = delta
 
         #set up matrix for the product formula
-        cdef complex c = cos(-tau/(2*delta*delta))
-        cdef complex s = 1j*sin(-tau/(2*delta*delta))
+        cdef double a = tau/(4*delta*delta) #fixed error from the lecture slides
+        cdef complex c = cos(a)
+        cdef complex s = 1j*sin(a)
         self.k_matrix = np.array([[c, s], [s, c]])
 
         #set up potential evolution factors:
-        #V = delta**-2 * (1 + delta**2 * V_i)
-        self.expV = np.exp(-1j*tau*(1/delta/delta + V)) #TODO: check this
+        self.expV = np.exp(-1j*tau*(1./delta/delta + V))
 
     def probability(self):
         return np.abs(np.asarray(self.psi, dtype=np.complex))**2
@@ -77,11 +77,16 @@ cdef class Schrodinger1:
             for j in range(1, self.L-1, 2):
                 mydot(self.k_matrix, self.psi[j:j+2])
 
-            #Second K_2 half step
+            # Because the K_1 matrix does not change with time, one could
+            # pull this second half step together with the first k_1 half step
+            # of the next step. This version looks nicer/cleaner.
+            #Second K_1 half step
             for j in range(0, self.L-1, 2):
                 mydot(self.k_matrix, self.psi[j:j+2])
 
     def evolve(self, int n):
+        '''Evolve the system for n time steps'''
         cdef int i
         for i in range(n):
             self.step()
+
